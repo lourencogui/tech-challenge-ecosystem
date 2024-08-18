b334efabfa616d819f3e9e384e13d9c32e679a44
Guilherme <guilhermelourenco1997@gmail.com>
2024-08-18 19:30:20 -0300

    create payable migration

---
+++ src/infra/database/migrations/1724019679618-CreatePayableEntity.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreatePayableEntity1724019679618 implements MigrationInterface {
+
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		const query = `CREATE TABLE "payables" (
+				"id" serial PRIMARY KEY NOT NULL,
+				"merchant_id" integer NOT NULL,
+				"amount" decimal NOT NULL,
+				"status" character varying(10) NOT NULL,
+				"sub_total" decimal NOT NULL,
+				"discount" decimal NOT NULL,
+				"total" decimal NOT NULL,
+				"due_date" timestamp without time zone NOT NULL,
+				"createdAt" timestamp without time zone NOT NULL,
+				"updatedAt" timestamp without time zone NOT NULL)`;
+		
+		await queryRunner.query(query);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		const query = `DROP TABLE IF EXISTS "payables"`;
+
+		await queryRunner.query(query);
+	}
+
+}
